cd19cb3967265e69b16fc3246e83ac7e6da801a9
이소영 <soyoung.dev@gmail.com>
2019-06-14 15:37:50 +0900

    feat: CONTACT ME 탭을 카테고리 목록 탭으로 수정

--- config.js
+++ config.js
@@ -1,33 +1,33 @@
 'use strict';
 
 module.exports = {
-  url: 'https://devsoyoung.github.io',
-  title: '큐트리 개발블로그',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
-  postsPerPage: 5,
-  googleAnalyticsId: 'UA-73379983-2',
-  menu: [
-    {
-      label: 'Articles',
-      path: '/'
-    },
-    {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
+    url: 'https://devsoyoung.github.io',
+    title: '큐트리 개발블로그',
+    copyright: '© All rights reserved.',
+    disqusShortname: '',
+    postsPerPage: 5,
+    googleAnalyticsId: 'UA-73379983-2',
+    menu: [
+        {
+            label: 'Articles',
+            path: '/'
+        },
+        {
+            label: 'About me',
+            path: '/pages/about'
+        },
+        {
+            label: 'Category',
+            path: '/categories'
+        }
+    ],
+    author: {
+        name: '큐트리',
+        photo: '/photo.jpg',
+        bio: '프론트엔드 개발과 어피치를 좋아합니다.',
+        contacts: {
+            email: 'soyoung.dev@gmail.com',
+            github: 'devSoyoung',
+        }
     }
-  ],
-  author: {
-    name: '큐트리',
-    photo: '/photo.jpg',
-    bio: '프론트엔드 개발과 어피치를 좋아합니다.',
-    contacts: {
-      email: 'soyoung.dev@gmail.com',
-      github: 'devSoyoung',
-    }
-  }
 };
