53c0ada9b52333072ac444df1cfdf69f45debdb5
cutelee <soyoung.dev@gmail.com>
2019-07-18 18:22:31 +0900

    태그, 작성자 스타일 수정 및 antd 추가

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,23 +1,26 @@
 // @flow
 import React from 'react';
+import { Avatar } from 'antd';
 import { getContactHref } from '../../../utils';
 import styles from './Author.module.scss';
 import { useSiteMetadata } from '../../../hooks';
 
+// TODO: 작성자 정보 형태 수정 (jekyll 블로그 모양으로 수정하기)
+
 const Author = () => {
   const { author } = useSiteMetadata();
-
   return (
     <div className={styles['author']}>
+      <Avatar className={styles['author__avatar']} src={author.photo} size={80} />
       <p className={styles['author__bio']}>
         {author.bio}
         <a
           className={styles['author__bio-twitter']}
-          href={getContactHref('twitter', author.contacts.twitter)}
+          href={getContactHref('github', author.contacts.github)}
           rel="noopener noreferrer"
           target="_blank"
         >
-          <strong>{author.name}</strong> on Twitter
+          <strong>{author.name}</strong> on Github
         </a>
       </p>
     </div>
--- src/components/Post/Tags/Tags.js
+++ src/components/Post/Tags/Tags.js
@@ -1,8 +1,8 @@
 // @flow
 import React from 'react';
 import { Link } from 'gatsby';
+import { Tag } from 'antd';
 import styles from './Tags.module.scss';
-import tag from '../../../assets/icon/tag.png';
 
 type Props = {
   tags: string[],
@@ -11,13 +11,15 @@ type Props = {
 
 const Tags = ({ tags, tagSlugs }: Props) => (
   <div className={styles['tags']}>
+    <span>태그 : </span>
     <ul className={styles['tags__list']}>
       {tagSlugs && tagSlugs.map((slug, i) => (
         <li className={styles['tags__list-item']} key={tags[i]}>
-          <img className={styles['tags__list-item-icon']} alt="tag-icon" src={tag} />
-          <Link to={slug} className={styles['tags__list-item-link']}>
+          <Tag>
+            <Link to={slug} className={styles['tags__list-item-link']}>
             {tags[i]}
-          </Link>
+            </Link>
+          </Tag>
         </li>
       ))}
     </ul>
