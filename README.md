7d536046cdedd487f8c4004f89b2ab9871410d40
cutelee <soyoung.dev@gmail.com>
2019-08-16 11:27:48 +0900

    README, config.js 수정

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# 개발 블로그
-
-이전의 Jekyll로 제작된 블로그를 Gatsby로 이전했습니다.
+# Dev Blog with Gatsby
--- config.js
+++ config.js
@@ -1,34 +1,34 @@
 'use strict';
 
 module.exports = {
-    url: 'https://devsoyoung.github.io',
-    title: '큐트리 개발블로그',
-    subtitle: '',
-    copyright: '© All rights reserved.',
-    disqusShortname: '',
-    postsPerPage: 5,
-    googleAnalyticsId: 'UA-73379983-2',
-    menu: [
-        {
-            label: 'Articles',
-            path: '/'
-        },
-        {
-            label: 'About me',
-            path: '/pages/about'
-        },
-        {
-            label: 'Category',
-            path: '/categories'
-        }
-    ],
-    author: {
-        name: '큐트리',
-        photo: '/photo.jpg',
-        bio: '프론트엔드 개발과 어피치를 좋아합니다.',
-        contacts: {
-            email: 'soyoung.dev@gmail.com',
-            github: 'devSoyoung',
-        }
+  url: 'https://devsoyoung.github.io',
+  title: 'Devlog',
+  subtitle: '',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 5,
+  googleAnalyticsId: 'UA-73379983-2',
+  menu: [
+    {
+      label: 'Posts',
+      path: '/'
+    },
+    {
+      label: 'Category',
+      path: '/categories'
+    },
+    {
+      label: 'Introduce',
+      path: '/pages/about'
     }
+  ],
+  author: {
+    name: '큐트리',
+    photo: '/photo.jpg',
+    bio: 'Front-end, React.js',
+    contacts: {
+      email: 'soyoung.dev@gmail.com',
+      github: 'devSoyoung',
+    }
+  }
 };
